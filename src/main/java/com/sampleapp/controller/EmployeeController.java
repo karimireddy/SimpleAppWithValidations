9b973a0edc68ba691d5c6fafc2c4a000003e179d
Jithendra Reddy K <jithendra.karimireddy@cesltd.com>
2018-09-22 22:08:16 +0530

    Added dto class and mapping

--- src/main/java/com/sampleapp/controller/EmployeeController.java
+++ src/main/java/com/sampleapp/controller/EmployeeController.java
@@ -1,13 +1,24 @@
 package com.sampleapp.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sampleapp.dto.EmployeeDto;
 
 @RestController
 public class EmployeeController {
-	
-	
-	
-	
-	
+
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	public String get() {
+		return "Hello World from Rest Controller";
+	}
+
+	@RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+	public String get(@RequestBody EmployeeDto dto) {
+		
+		return dto.toString();
+	}
 	
 }
