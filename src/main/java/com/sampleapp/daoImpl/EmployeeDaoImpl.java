601a555e82d3a7ea0272d041aff6f1b89f1874ae
Jithendra Reddy K <jithendra.karimireddy@cesltd.com>
2018-09-23 23:23:03 +0530

    T

--- src/main/java/com/sampleapp/daoImpl/EmployeeDaoImpl.java
+++ src/main/java/com/sampleapp/daoImpl/EmployeeDaoImpl.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
-import org.hibernate.SessionFactory;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
